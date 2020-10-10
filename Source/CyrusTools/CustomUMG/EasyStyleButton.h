32d870e425cefd1714d2f674041cd7d74ad59a9a
unknown <chenchuntao@ihomeint.com>
2020-10-10 10:46:24 +0800

    优化EasyStyleButton

--- Source/CyrusTools/CustomUMG/EasyStyleButton.h
+++ Source/CyrusTools/CustomUMG/EasyStyleButton.h
@@ -1,173 +1,47 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
+// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Components/ContentWidget.h"
+#include "Components/Button.h"
 #include "EasyStyleButton.generated.h"
 
-class SButton;
 class USlateWidgetStyleAsset;
-
-DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnButtonClickedEvent);
-DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnButtonPressedEvent);
-DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnButtonReleasedEvent);
-DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnButtonHoverEvent);
 
 /**
  * 能使用简便的方式设置样式的按钮，继承于UButton
  * 除了样式设置方式不同，其它都与UButton一致
  */
 UCLASS()
-class CYRUSTOOLS_API UEasyStyleButton : public UContentWidget
+class CYRUSTOOLS_API UEasyStyleButton : public UButton
 {
-	GENERATED_UCLASS_BODY()
+	GENERATED_BODY()
 public:
 
-	UPROPERTY(EditAnywhere, Category = "Appearance", meta = (AllowPrivateAccess = "true", DisplayThumbnail = "true", AllowedClasses = "Texture,MaterialInterface,SlateTextureAtlasInterface", DisallowedClasses = "MediaTexture"))
+	UPROPERTY(EditAnywhere, Category = "Appearance Easy", meta = (AllowPrivateAccess = "true", DisplayThumbnail = "true", AllowedClasses = "Texture,MaterialInterface,SlateTextureAtlasInterface", DisallowedClasses = "MediaTexture"))
 		UObject* NormalStyle;
 
-	UPROPERTY(EditAnywhere, Category = "Appearance", meta = (AllowPrivateAccess = "true", DisplayThumbnail = "true", AllowedClasses = "Texture,MaterialInterface,SlateTextureAtlasInterface", DisallowedClasses = "MediaTexture"))
+	UPROPERTY(EditAnywhere, Category = "Appearance Easy", meta = (AllowPrivateAccess = "true", DisplayThumbnail = "true", AllowedClasses = "Texture,MaterialInterface,SlateTextureAtlasInterface", DisallowedClasses = "MediaTexture"))
 		UObject* HoveredStyle;
 
-	UPROPERTY(EditAnywhere, Category = "Appearance", meta = (sRGB = "true"))
+	UPROPERTY(EditAnywhere, Category = "Appearance Easy", meta = (AllowPrivateAccess = "true", DisplayThumbnail = "true", AllowedClasses = "Texture,MaterialInterface,SlateTextureAtlasInterface", DisallowedClasses = "MediaTexture"))
+		UObject* PressedStyle;
+
+	UPROPERTY(EditAnywhere, Category = "Appearance Easy", meta = (sRGB = "true"))
+		FLinearColor HoveredColor = FLinearColor::White;
+
+	UPROPERTY(EditAnywhere, Category = "Appearance Easy", meta = (sRGB = "true"))
 		FLinearColor PressedColor = FLinearColor::White;
 
-	UPROPERTY(EditAnywhere, Category = "Appearance", meta = (DisplayName = "Size"))
+	UPROPERTY(EditAnywhere, Category = "Appearance Easy", meta = (DisplayName = "Size"))
 		FVector2D ButtonSize = FVector2D(SlateBrushDefs::DefaultImageSize, SlateBrushDefs::DefaultImageSize);
-
-public:
-
-	/** The template style asset, used to seed the mutable instance of the style. */
-	UPROPERTY()
-		USlateWidgetStyleAsset* Style_DEPRECATED;
-
-	/** The button style used at runtime */
-	UPROPERTY(/*EditAnywhere, */BlueprintReadWrite, Category = "Appearance", meta = (DisplayName = "Style"))
-		FButtonStyle WidgetStyle;
-
-	/** The color multiplier for the button content */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Appearance", meta = (sRGB = "true"))
-		FLinearColor ColorAndOpacity;
-
-	/** The color multiplier for the button background */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Appearance", meta = (sRGB = "true"))
-		FLinearColor BackgroundColor;
-
-	/** The type of mouse action required by the user to trigger the buttons 'Click' */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Interaction", AdvancedDisplay)
-		TEnumAsByte<EButtonClickMethod::Type> ClickMethod;
-
-	/** The type of touch action required by the user to trigger the buttons 'Click' */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Interaction", AdvancedDisplay)
-		TEnumAsByte<EButtonTouchMethod::Type> TouchMethod;
-
-	/** The type of keyboard/gamepad button press action required by the user to trigger the buttons 'Click' */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Interaction", AdvancedDisplay)
-		TEnumAsByte<EButtonPressMethod::Type> PressMethod;
-
-	/** Sometimes a button should only be mouse-clickable and never keyboard focusable. */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Interaction")
-		bool IsFocusable;
-
-public:
-
-	/** Called when the button is clicked */
-	UPROPERTY(BlueprintAssignable, Category = "Button|Event")
-		FOnButtonClickedEvent OnClicked;
-
-	/** Called when the button is pressed */
-	UPROPERTY(BlueprintAssignable, Category = "Button|Event")
-		FOnButtonPressedEvent OnPressed;
-
-	/** Called when the button is released */
-	UPROPERTY(BlueprintAssignable, Category = "Button|Event")
-		FOnButtonReleasedEvent OnReleased;
-
-	UPROPERTY(BlueprintAssignable, Category = "Button|Event")
-		FOnButtonHoverEvent OnHovered;
-
-	UPROPERTY(BlueprintAssignable, Category = "Button|Event")
-		FOnButtonHoverEvent OnUnhovered;
-
-public:
-
-	/** Sets the color multiplier for the button background */
-	UFUNCTION(BlueprintCallable, Category = "Button|Appearance")
-		void SetStyle(const FButtonStyle& InStyle);
-
-	/** Sets the color multiplier for the button content */
-	UFUNCTION(BlueprintCallable, Category = "Button|Appearance")
-		void SetColorAndOpacity(FLinearColor InColorAndOpacity);
-
-	/** Sets the color multiplier for the button background */
-	UFUNCTION(BlueprintCallable, Category = "Button|Appearance")
-		void SetBackgroundColor(FLinearColor InBackgroundColor);
-
-	/**
-	 * Returns true if the user is actively pressing the button.  Do not use this for detecting 'Clicks', use the OnClicked event instead.
-	 *
-	 * @return true if the user is actively pressing the button otherwise false.
-	 */
-	UFUNCTION(BlueprintCallable, Category = "Button")
-		bool IsPressed() const;
-
-	UFUNCTION(BlueprintCallable, Category = "Button")
-		void SetClickMethod(EButtonClickMethod::Type InClickMethod);
-
-	UFUNCTION(BlueprintCallable, Category = "Button")
-		void SetTouchMethod(EButtonTouchMethod::Type InTouchMethod);
-
-	UFUNCTION(BlueprintCallable, Category = "Button")
-		void SetPressMethod(EButtonPressMethod::Type InPressMethod);
-
 public:
 
 	//~ Begin UWidget Interface
 	virtual void SynchronizeProperties() override;
 	//~ End UWidget Interface
 
-	//~ Begin UVisual Interface
-	virtual void ReleaseSlateResources(bool bReleaseChildren) override;
-	//~ End UVisual Interface
-
-	//~ Begin UObject Interface
-	virtual void PostLoad() override;
-	//~ End UObject Interface
-
 #if WITH_EDITOR
 	virtual const FText GetPaletteCategory() override;
 #endif
-
-protected:
-
-	// UPanelWidget
-	virtual UClass* GetSlotClass() const override;
-	virtual void OnSlotAdded(UPanelSlot* Slot) override;
-	virtual void OnSlotRemoved(UPanelSlot* Slot) override;
-	// End UPanelWidget
-
-protected:
-	/** Handle the actual click event from slate and forward it on */
-	virtual FReply SlateHandleClicked();
-	void SlateHandlePressed();
-	void SlateHandleReleased();
-	void SlateHandleHovered();
-	void SlateHandleUnhovered();
-
-protected:
-	//~ Begin UWidget Interface
-	virtual TSharedRef<SWidget> RebuildWidget() override;
-#if WITH_EDITOR
-	virtual TSharedRef<SWidget> RebuildDesignWidget(TSharedRef<SWidget> Content) override { return Content; }
-#endif
-	//~ End UWidget Interface
-
-#if WITH_ACCESSIBILITY
-	virtual TSharedPtr<SWidget> GetAccessibleWidget() const override;
-#endif
-
-protected:
-	/** Cached pointer to the underlying slate button owned by this UWidget */
-	TSharedPtr<SButton> MyButton;
 };
